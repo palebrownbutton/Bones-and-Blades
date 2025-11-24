11f72ffd49f24d73d5ef5f7685385798dca5e711
palebrownbutton <simonavatcheva@hotmail.com>
2025-11-24 22:09:40 +0000

    Added images for available items in the inventory
    Will make knight standing in the middle as a view of the customisable actions the player is doing.

--- Inventory.py
+++ Inventory.py
@@ -20,12 +20,30 @@ class Item(AnimatedSprite):
         self.type = new_type
 
 background = StillImage(0, 0, 800, 800, "select_background.png")
-knight_custom = Item( "Knight_1/Idle.png", 100, 100, 128, 128, "sword")
+knight_custom = Item( "Knight_1/Idle.png", 250, -300, 128, 128, "sword")
 knight_custom.resize(640, 640)
+
+wooden_sword = StillImage(100, 370, 200, 200, "wooden_sword.png")
+iron_sword = StillImage(300, 400, 150, 150, "iron_sword.png")
+diamond_sword = StillImage(500, 410, 120, 120, "diamond_sword.png")
+
+wooden_shield = StillImage(100, 600, 128, 128, "wooden_shield.png")
+iron_sheild = StillImage(300, 600, 128, 128, "iron_shield.png")
+diamond_sheild = StillImage(500, 600, 128, 128, "diamond_shield.png")
 
 def inventory(window):
 
+    mouse_x, mouse_y = mouse.get_pos()
+
     background.draw(window)
     knight_custom.draw(window)
+
+    wooden_sword.draw(window)
+    iron_sword.draw(window)
+    diamond_sword.draw(window)
+
+    wooden_shield.draw(window)
+    iron_sheild.draw(window)
+    diamond_sheild.draw(window)
 
     return True
